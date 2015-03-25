e5964bf26dd55375b10b51f4f40c273c5620cf36
awsmdavid <awsmdavid@gmail.com>
2015-03-25 17:21:16 -0400

    parse list

--- blog/views.py
+++ blog/views.py
@@ -10,6 +10,10 @@ from django.http import HttpResponse
 from django.shortcuts import render_to_response
 from django.core.context_processors import csrf
 
+
+articles = ["a", "the", "an"]
+common_words = ["i", "you", "he", "she", "it", "they", "is"]
+
 def index(request):
     return render(request, 'blog/index.html')
 
@@ -24,30 +28,34 @@ def results(request):
         words_without_punc = text.translate(string.maketrans("",""), string.punctuation)
         # generate iterable of words
         words = words_without_punc.split()
-        # count words and generate key value pair
-        words_count = Counter(words)
 
-        articles = ["a", "the", "an"]
-        # remove articles
+        # list comp to lower case for each word
+        all_words = [word.lower() for word in words]
+
+        # count words and generate key value pair
+        all_words_count = Counter(all_words)
+
+        # remove articles and common words
         for a in articles:
-            del words_count[a]
-        words_and_count_array = words_count
+            del all_words_count[a]
+
+        for a in common_words:
+            del all_words_count[a]
+        words_and_count_array = all_words_count
 
         # returns n most common elements in counter, converts counter to dict
-        n=3
-        top_words = words_and_count_array.most_common()[:3]
+        # TODO: be smarter about number of results, ignore ones with count of 1, perhaps if they are not used consecutively
+        number_of_results=5
+        top_words = words_and_count_array.most_common()[:number_of_results]
 
+        # generate synonyms for word
+        synonyms = "thing, that, it, yeah"
 
-        # define top words
-        # takes first tuple in words_count.most_common, and then takes the first item (the word) of that tuple
-        top_word = words_and_count_array.most_common()[0][0]
-        # takes count of that tuple
-        count_of_top_word = words_and_count_array.most_common()[0][1]
+        new_list = [[(n[0], n[1],synonyms)] for n in top_words]
 
-
-        essay_data = essayData(text=text, word_list = words, words_count = words_count, top_words = top_word)
-        top_word = word(word=top_word, count=count_of_top_word)
-        return render(request, 'blog/results.html', { 'search_results': essay_data, 'top_words': words_and_count_array.most_common()[:3]})
+        essay_data = essayData(text=text, word_list = words, words_count = all_words_count)
+        # top_word = word(word=top_word, count=count_of_top_word)
+        return render(request, 'blog/results.html', { 'search_results': essay_data, 'top_words': top_words, 'new_list':new_list})
     return render(request, 'blog/index.html')
 
 
