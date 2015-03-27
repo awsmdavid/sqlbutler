a3c73593a1ede1d08de0593650044605249dc330
awsmdavid <awsmdavid@gmail.com>
2015-03-26 23:55:37 -0400

    toggle synonym suggestions

--- js/app.js
+++ js/app.js
@@ -1,3 +1,19 @@
 // Foundation JavaScript
 // Documentation can be found at: http://foundation.zurb.com/docs
 $(document).foundation();
+$('.toggle-expand').click(function(){
+	$(this).siblings(".word-metadata").toggle(200, function() {
+		if ($(this).siblings(".toggle-expand").html() == "[+]"){
+			$(this).siblings(".toggle-expand").html("[-]");
+		}
+		else{
+			$(this).siblings(".toggle-expand").html("[+]");
+		}
+	});
+});
+	// .css( "display", "inline" );
+// $('.home').click(function() {
+	// $(this).css('background-image', 'url(images/tabs3.png)');
+// });
+
+// document.getElementById("toggle-expand").onclick = function () { $(this).css('color','red'); };
